9f3f9f784e847d391e3a792ee6282876adf0e35d
GitBib <15717621+GitBib@users.noreply.github.com>
2023-07-11 13:59:35 +0300

    fix

--- getPSSH.py
+++ getPSSH.py
@@ -1,47 +1,34 @@
-import requests, xmltodict, json
+import requests
+import xml.etree.ElementTree as ET
+import base64
 
-def get_pssh(mpd_url):
-    pssh = ''
-    try:
-        r = requests.get(url=mpd_url)
+def get_pssh(input):
+    # Initialize the list of pssh data
+    pssh_data_list = []
+
+    # Check if the input is a URL or a file path
+    if input.startswith('http://') or input.startswith('https://'):
+        # Make a request to the URL
+        r = requests.get(url=input)
         r.raise_for_status()
-        xml = xmltodict.parse(r.text)
-        mpd = json.loads(json.dumps(xml))
-        periods = mpd['MPD']['Period']
-    except Exception as e:
-        pssh = input(f'\nUnable to find PSSH in MPD: {e}. \nEdit getPSSH.py or enter PSSH manually: ')
-        return pssh
-    try: 
-        if isinstance(periods, list):
-            for idx, period in enumerate(periods):
-                if isinstance(period['AdaptationSet'], list):
-                    for ad_set in period['AdaptationSet']:
-                        if ad_set['@mimeType'] == 'video/mp4':
-                            try:
-                                for t in ad_set['ContentProtection']:
-                                    if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
-                                        pssh = t["cenc:pssh"]
-                            except Exception:
-                                pass   
-                else:
-                    if period['AdaptationSet']['@mimeType'] == 'video/mp4':
-                            try:
-                                for t in period['AdaptationSet']['ContentProtection']:
-                                    if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
-                                        pssh = t["cenc:pssh"]
-                            except Exception:
-                                pass   
-        else:
-            for ad_set in periods['AdaptationSet']:
-                    if ad_set['@mimeType'] == 'video/mp4':
-                        try:
-                            for t in ad_set['ContentProtection']:
-                                if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
-                                    pssh = t["cenc:pssh"]
-                        except Exception:
-                            pass   
-    except Exception:
-        pass                      
-    if pssh == '':
-        pssh = input('Unable to find PSSH in mpd. Edit getPSSH.py or enter PSSH manually: ')
-    return pssh
+        # Parse the XML data from the response
+        root = ET.fromstring(r.text)
+    else:
+        # Read the file
+        with open(input, 'r') as file:
+            file_data = file.read()
+        # Parse the XML data from the file
+        root = ET.fromstring(file_data)
+
+    # Find all the cenc:pssh elements and extract their content
+    for elem in root.iter():
+        if 'pssh' in elem.tag:
+            pssh_data = elem.text
+            # If the pssh data is base64 encoded, let's decode it
+            try:
+                pssh_data = base64.b64decode(pssh_data).decode('utf-8')
+            except:
+                pass
+            pssh_data_list.append(pssh_data)
+
+    return pssh_data_list
